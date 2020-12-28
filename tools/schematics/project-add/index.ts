e576fa0f3b142f760a9884b981bd38bbdaf1ed77
zhangjindong <zhangjindong@gcstgroup.com>
2020-12-28 17:50:27 +0800

    add schematic project-add

--- tools/schematics/project-add/index.ts
+++ tools/schematics/project-add/index.ts
@@ -4,9 +4,11 @@ export default function (schema: any): Rule {
   return chain([
     externalSchematic('@nrwl/angular', 'application', {
       name: schema.name,
+      style: 'scss',
+      routing: true,
     }),
     externalSchematic('@nrwl/nest', 'application', {
-      name: schema.name,
+      name: schema.name + '-node',
     }),
   ]);
 }
